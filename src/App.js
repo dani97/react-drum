4ecc18998caa285d4a6e3c75a5a80c8cc5be0fcc
Christopher Daniel <christopherdaniel13397@gmail.com>
2019-03-30 16:29:56 +0530

    app converted to pure functional

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import  Drum  from './Components/Drum/Drum';
+import Drum from './Components/Drum/Drum';
 import KeyStateProvider from './Components/KeyStateProvider/KeyStateProvider';
 
 
-class App extends Component {
-  
-  render() {
-    return (
-      <KeyStateProvider>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-          </header>
-          <Drum/>
-        </div>
-      </KeyStateProvider>
-    );
-  }
+const App = () => {
+  return (
+    <KeyStateProvider>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <Drum />
+      </div>
+    </KeyStateProvider>
+  );
 }
 
 export default App;
